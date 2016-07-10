d010c01595cb5728349c8f2c9e51970554ac8974
anirudt <anirudt@gmail.com>
2016-07-10 14:43:36 +0530

    fixed SP bug

--- server.py
+++ server.py
@@ -195,18 +195,20 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             bool_ack_help      = form['ack_help'].value
 
         list_contacts = []
+        unproc_list_contacts = []
         review = ""
         if form.has_key('contact_file') and form['contact_file'].value != '':
             ret = json.loads(form['contact_file'].value)
             unproc_list_contacts = ret
-        
-        tmp = ""
+            tmp = ""
+            if len(unproc_list_contacts) > 0:
+                for contact in unproc_list_contacts:
+                    tmp = contact.replace("+", "")
+                    tmp = tmp.replace("-", "")
+                    tmp = tmp.replace(" ", "")
+                    list_contacts.append(tmp[-10:])
+
         # Processing for standardising the contacts.
-        for contact in unproc_list_contacts:
-            tmp = contact.replace("+", "")
-            tmp = tmp.replace("-", "")
-            tmp = tmp.replace(" ", "")
-            list_contacts.append(tmp[-10:])
         # We will need location and contact number (ID) for any action!
         px, py = "", ""
         if form.has_key('px') and form['px'].value != "":
@@ -227,7 +229,7 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         ret = {}
         print bool_contacts_send, bool_fetch_friends, bool_fetch_reviews, bool_give_reviews, bool_sos_call_low, bool_sos_call_med, bool_sos_call_high, \
-        bool_ack_help, self_id, location, review, list_contacts
+                bool_ack_help, self_id, location, review, list_contacts
         a, b = process_args([bool_contacts_send, bool_fetch_friends, bool_fetch_reviews, bool_give_reviews, bool_sos_call_low, bool_sos_call_med, bool_sos_call_high, bool_ack_help], self_id, location, review, list_contacts)
 
         # TODO: Check if null
@@ -253,7 +255,7 @@ def main(timeout_mins):
     print "@rochacbruno Python http server version 0.1 (for testing purposes only)"
     print "Serving at: http://%(interface)s:%(port)s" % dict(interface=I or "localhost", port=PORT)
     httpd.serve_forever()
-    
+
     tick = time.time()
     thread.start_new_thread(httpd.serve_forever,())
     while 1:
